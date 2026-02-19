ac77540909b5ae26179862634a89a2918dee6c87
joe933212-pixel <joe933212@gmail.com>
2026-02-19 04:52:36 +0300

    Create Polygon Web3 client interactions methods

--- backend/blockchain.py
+++ backend/blockchain.py
@@ -1,37 +1,54 @@
-import web3
+from web3 import Web3
 
-class PolygonClient:
-    def __init__(self, rpc_url):
-        self.web3 = web3.Web3(web3.Web3.HTTPProvider(rpc_url))
-        if not self.web3.isConnected():
-            raise Exception("Unable to connect to the Polygon network")
+class PolygonWeb3Client:
+    def __init__(self, infura_url):
+        self.web3 = Web3(Web3.HTTPProvider(infura_url))
 
     def get_balance(self, address):
-        return self.web3.fromWei(self.web3.eth.get_balance(address), 'ether')
+        """
+        Get the balance of an account in Wei.
+        Args:
+            address (str): The address of the account.
+        Returns:
+            int: The balance in Wei.
+        """
+        balance = self.web3.eth.get_balance(address)
+        return balance
 
-    def send_transaction(self, from_address, to_address, amount, private_key):
-        nonce = self.web3.eth.getTransactionCount(from_address)
-        gas_price = self.web3.eth.gas_price
-        tx = {
-            'nonce': nonce,
+    def send_transaction(self, from_address, to_address, value, private_key):
+        """
+        Send a transaction from one account to another.
+        Args:
+            from_address (str): The sender's address.
+            to_address (str): The receiver's address.
+            value (int): The amount to send in Wei.
+            private_key (str): The private key of the sender's account.
+        Returns:
+            str: Transaction hash.
+        """
+        transaction = {
             'to': to_address,
-            'value': self.web3.toWei(amount, 'ether'),
+            'value': value,
             'gas': 2000000,
-            'gasPrice': gas_price,
+            'gasPrice': self.web3.toWei('50', 'gwei'),
+            'nonce': self.web3.eth.get_transaction_count(from_address),
         }
+        signed_txn = self.web3.eth.account.sign_transaction(transaction, private_key)
+        txn_hash = self.web3.eth.send_raw_transaction(signed_txn.rawTransaction)
+        return txn_hash.hex()
 
-        signed_tx = self.web3.eth.account.sign_transaction(tx, private_key)
-        tx_hash = self.web3.eth.sendRawTransaction(signed_tx.rawTransaction)
-        return self.web3.toHex(tx_hash)
+    def contract_interaction(self, contract_address, abi, method_name, *args):
+        """
+        Interact with a smart contract.
+        Args:
+            contract_address (str): The contract's address.
+            abi (list): The contract's ABI.
+            method_name (str): The method to call.
+            *args: Arguments for the method.
+        Returns:
+            any: The result of the method call.
+        """
+        contract = self.web3.eth.contract(address=contract_address, abi=abi)
+        method = getattr(contract.functions, method_name)
+        return method(*args).call()  
 
-    def estimate_gas(self, tx):
-        return self.web3.eth.estimateGas(tx)
-
-    def optimize_gas(self, from_address, to_address, amount):
-        tx = {
-            'from': from_address,
-            'to': to_address,
-            'value': self.web3.toWei(amount, 'ether'),
-            'gas': 2000000,
-        }
-        return self.estimate_gas(tx)
